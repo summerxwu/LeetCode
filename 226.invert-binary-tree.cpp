8dbf7cf0edc8ed75092045bb604608a7240a63ab
summerxwu <wuxia900717@gmail.com>
2022-12-28 14:36:38 +0800

    finish the first week of the binary tree

---
+++ 226.invert-binary-tree.cpp
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (73.22%)
+ * Likes:    10600
+ * Dislikes: 151
+ * Total Accepted:    1.4M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  // recursive edition
+  TreeNode *invertTree(TreeNode *root) {
+    if (root == nullptr) {
+      return root;
+    }
+    // invert left
+    this->invertTree(root->left);
+    this->invertTree(root->right);
+    TreeNode *it = root->left;
+    root->left = root->right;
+    root->right = it;
+    return root;
+  }
+};
+// @lc code=end
